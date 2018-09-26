283ab40667de4e15b6c3460d9a3b0665bceeed47
Brandon <brandonlehmann@gmail.com>
2018-09-26 18:14:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 module.exports.TurtleCoind = require('./lib/turtlecoind-rpc.js')
 module.exports.TurtleService = require('./lib/service-rpc.js')
 module.exports.Client = require('./lib/client-rpc.js')
+
 // These exports will be deprecated in a future version.
 // Continue to use them at your own risk.
 module.exports.Walletd = require('./lib/service-rpc.js')
